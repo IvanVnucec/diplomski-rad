aa81be6c63ee782e66bd02ac6bd5475e17de5b50
Ivan Vnucec <vnucec.ivan@gmail.com>
2022-06-24 15:39:36 +0200

    add ang vel dist reg png

---
+++ other/plot_meas_ang_vel_dist.m
@@ -0,0 +1,43 @@
+clear all;
+close all;
+
+% load and prepare ang vel meas
+meas = csvread('measurements/ang_vel_regulation_disturb.csv');
+
+meas = meas(1030:1600,:,:);
+
+wz_ref = meas(:,1);
+wz_ref = wz_ref ./ 1000.0; % to rad/s
+
+wz = meas(:,2);
+wz = wz ./ 1000.0; % to rad/s
+
+cntrl = meas(:,3);
+
+% create time vector
+t = 1:length(wz_ref);
+t = t ./ 10.0; % convert to seconds
+t = t';
+
+w = [wz_ref, wz];
+
+% plot
+font_size_title = 15;
+font_size = 12;
+
+hf = figure();
+[hax, h1, h2] = plotyy(t, w, t, cntrl);
+xlabel("Vrijeme [s]");
+ylabel(hax(1), "Kutna brzina oko z-osi [rad/s]");
+ylabel(hax(2), "Izlaz iz regulatora (DC) [%]");
+title("Odziv sustava na vanjsku smetnju kutne brzine", "fontsize", font_size_title);
+set(hax(1), "fontsize", font_size)
+set(hax(2), "fontsize", font_size)
+set(h1, "linewidth", 2)
+set(h2, "linewidth", 1)
+ylim(hax(1), [-1.1 1.1])
+ylim(hax(2), [-100 100])
+legend('Referentna kutna brzina', 'Izmjerena kutna brzina', 'Izlaz regulatora')
+
+% printing to pdf
+print(hf, "ang_vel_reg_dist.png", '-dpng');
